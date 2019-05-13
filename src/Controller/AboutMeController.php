404da932e165ed38f3ce6514ea349e4e26d381dd
sidczak <sidczak@clickmeeting.com>
2019-05-13 14:08:34 +0200

    sf4 - Step2 - Create new StartController - about

--- src/Controller/AboutMeController.php
+++ src/Controller/AboutMeController.php
@@ -2,12 +2,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AboutMeController extends AbstractController
 {
-    public function index(Request $reques)
+    /**
+     * @Route("/about-me", name="about_me")
+     */
+    public function index()
     {
-        return $this->render('aboutMe/index.html.twig');
+        //return $this->render('about_me/index.html.twig');
+        return $this->render('about_me/index.html.twig', [
+            'controller_name' => 'AboutMeController',
+        ]);
     }
 }
