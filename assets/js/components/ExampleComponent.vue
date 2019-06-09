ceacec4c07e5285385524d5f65b98cd4cad78e90
Szymon Idczak <szymon.idczak@gmail.com>
2019-06-09 19:51:16 +0200

    Vue - Step5 - Vuetify

--- assets/js/components/ExampleComponent.vue
+++ assets/js/components/ExampleComponent.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>Dynamic prop value: {{ myprop }} {{ heading }} {{ Number(index) + 2 }}</p>
+    <div>
+        <p>Dynamic prop value: {{ myprop }} {{ heading }} {{ Number(index) + 2 }}</p>
+        <h3>Komponent vuetify poniżej:</h3>
+        <v-btn color="success">Success</v-btn>
+        <v-btn color="error">Error</v-btn>
+        <v-btn color="warning">Warning</v-btn>
+        <v-btn color="info">Info</v-btn>
+    </div>
 </template>
 <script>
     export default {
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+new Vue({ el: '#app' });
 import VueRouter from 'vue-router';
 import vueCustomElement from 'vue-custom-element';
 
 import '../css/home.scss';
+
+// vuetify style
+import 'vuetify/dist/vuetify.min.css'
 
 import ExampleComponent from './components/ExampleComponent.vue';
 import EduCounter from './components/EduCounter.vue';
