e4afd34b3b63336ff5b698f0f4d6f4fa5b6b9bb6
sidczak <sidczak@clickmeeting.com>
2019-06-12 17:48:06 +0200

    vuetify menu

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify'
-Vue.use(Vuetify)
+// Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  iconfont: 'md'
+})
 new Vue({ el: '#app' });
 import VueRouter from 'vue-router';
 import vueCustomElement from 'vue-custom-element';
@@ -9,6 +12,7 @@ import '../css/home.scss';
 
 // vuetify style
 import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 import ExampleComponent from './components/ExampleComponent.vue';
 import EduCounter from './components/EduCounter.vue';
