bc924e08b9532b3de3e3ed64b57be9464fa27a18
sidczak <sidczak@clickmeeting.com>
2019-09-23 16:37:40 +0200

    resize

--- assets/js/bs-components/Resize.vue
+++ assets/js/bs-components/Resize.vue
@@ -1,13 +1,9 @@
-<template>
-    <section class="section has-text-centered">
-        <h1 class="title is-1">
-            Your Window
-        </h1>
-        <h2 class="title is-2">
-            Width: {{ window.width }},
-            Height: {{ window.height }}
-        </h2>
-    </section>
+<template lang="pug">
+    section.d-flex.align-items-center.section-py-80.section-min-h-100
+        b-container
+            b-row.text-center(v-for="n in 1" :key="n.id")
+                b-col
+                    h1.mb-0 Your Window:<br/> Width: {{ window.width }}, Height: {{ window.height }}
 </template>
 <script>
     export default {
@@ -35,16 +31,3 @@
         }
     };
 </script>
-<style lang="scss" scoped>
-section.section {
-  height: 100%;
-  border: 1px solid red;
-}
-
-section.section {
-  display: flex;
-  flex-flow: column;
-  justify-content: center;
-  align-items: center;
-}
-</style>
