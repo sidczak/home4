6630f0299783b9579759d6f7aa1932a7405be3ca
sidczak <sidczak@clickmeeting.com>
2019-10-08 17:09:17 +0200

    resize udate

--- assets/js/bs-components/Resize.vue
+++ assets/js/bs-components/Resize.vue
@@ -4,6 +4,25 @@
             b-row.text-center(v-for="n in 1" :key="n.id")
                 b-col
                     h1.mb-0 Your Window:<br/> Width: {{ window.width }}, Height: {{ window.height }}
+                    .box.text-center
+                        | boxOffset: {{ boxOffset.width }} x {{ boxOffset.height }}
+                        br
+                        | boxOffsetTop: {{ boxOffset.top }}
+                        br
+                        | boxOffsetLeft: {{ boxOffset.left }}
+                        br
+                        | boxClient: {{ boxClient.width }} x {{ boxClient.height }}
+                        br
+                        | boxClientTop: {{ boxClient.top }}
+                        br
+                        | boxClientLeft: {{ boxClient.left }}
+                        br
+                        | boxScroll: {{ boxScroll.width }} x {{ boxScroll.height }}
+                        br
+                        | boxScrollTop: {{ boxScroll.top }}
+                        br
+                        | boxScrollLeft: {{ boxScroll.left }}
+
 </template>
 <script>
     export default {
@@ -13,6 +32,24 @@
                 window: {
                     width: 0,
                     height: 0
+                },
+                boxOffset: {
+                    width: 0,
+                    height: 0,
+                    top: 0,
+                    left: 0
+                },
+                boxClient: {
+                    width: 0,
+                    height: 0,
+                    top: 0,
+                    left: 0
+                },
+                boxScroll: {
+                    width: 0,
+                    height: 0,
+                    top: 0,
+                    left: 0
                 }
             }
         },
@@ -27,7 +64,33 @@
             handleResize() {
                 this.window.width = window.innerWidth;
                 this.window.height = window.innerHeight;
+
+                // const element = document.getElementById('#box');
+                const element = document.querySelector('.box');
+
+                this.boxOffset.width = element.offsetWidth;
+                this.boxOffset.height = element.offsetHeight;
+                this.boxOffset.top = element.offsetTop;
+                this.boxOffset.left = element.offsetLeft;
+
+                this.boxClient.width = element.clientWidth;
+                this.boxClient.height = element.clientHeight;
+                this.boxClient.top = element.clientTop;
+                this.boxClient.left = element.clientLeft;
+
+                this.boxScroll.width = element.scrollWidth;
+                this.boxScroll.height = element.scrollHeight;
+                this.boxScroll.top = element.scrollTop;
+                this.boxScroll.left = element.scrollLeft;
             }
         }
     };
 </script>
+<style lang="scss" scoped>
+.box {
+    background: red;
+    border: 10px solid blue;
+    margin: 20px;
+    padding: 30px;
+}
+</style>
