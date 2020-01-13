325159764965927a260116c6a8fb8756dccd2ef7
sidczak <sidczak@clickmeeting.com>
2020-01-13 17:22:20 +0100

    contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(Request $reques)
+    {
+        return $this->render('contact/index.html.twig');
+    }
+}
