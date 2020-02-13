e583c2e67165203ee0c0f1c1f4f474500b052b0e
sidczak <sidczak@clickmeeting.com>
2020-02-13 12:41:52 +0100

    contact form

---
+++ src/Entity/Contact.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+class Contact
+{
+    private $id;
+
+    private $name;
+
+    private $email;
+
+    private $subject;
+
+    private $description;
+
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+    
+    public function prePersist()
+    {
+        $this->createdAt = new \DateTime();
+    }
+}
