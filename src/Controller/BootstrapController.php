d4b2b1011dd2f387f45025585ed71bc3261c00ee
sidczak <sidczak@clickmeeting.com>
2020-02-21 17:21:42 +0100

    bootstrap page

--- src/Controller/BootstrapController.php
+++ src/Controller/BootstrapController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/bootstrap/examples", defaults={"_project": "bootstrap"})
+ * @Route("/bootstrap", defaults={"_project": "bootstrap"})
  */
 class BootstrapController extends AbstractController
 {
@@ -15,23 +15,39 @@ class BootstrapController extends AbstractController
      */
     public function index()
     {
-        return $this->render('bootstrap/index.html.twig');
+        return $this->render('bootstrap/start/index.html.twig');
     }
     
     /**
-     * @Route("/album", name="examples_album")
+     * @Route("/examples/album", name="examples_album")
      */
     public function album()
     {
-        return $this->render('bootstrap/album.html.twig');
+        return $this->render('bootstrap/examples/album.html.twig');
     }
 
     /**
-     * @Route("/pricing", name="examples_pricing")
+     * @Route("/examples/pricing", name="examples_pricing")
      */
     public function pricing()
     {
-        return $this->render('bootstrap/pricing.html.twig');
+        return $this->render('bootstrap/examples/pricing.html.twig');
+    }
+    
+    /**
+     * @Route("/components/buttons", name="components_buttons")
+     */
+    public function buttons()
+    {
+        return $this->render('bootstrap/components/buttons.html.twig');
+    }
+
+    /**
+     * @Route("/components/section", name="components_section")
+     */
+    public function section()
+    {
+        return $this->render('bootstrap/components/section.html.twig');
     }
 
     /**
