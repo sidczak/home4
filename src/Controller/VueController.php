0a70442af61814ad732a3a87e357c8a6db54015b
Szymon Idczak <szymon.idczak@gmail.com>
2020-03-01 21:11:33 +0100

    sidebar menu vue examples

--- src/Controller/VueController.php
+++ src/Controller/VueController.php
@@ -33,4 +33,38 @@ class VueController extends AbstractController
     {
         return $this->render('vue/examples/browser.html.twig');
     }
+
+    /**
+     * @Route("/examples/sound-card", name="vue_examples_sound_card")
+     */
+    public function soundCard()
+    {
+        return $this->render('vue/examples/sound_card.html.twig');
+    }
+
+    
+
+    /**
+     * @Route("/examples/resize", name="vue_examples_resize")
+     */
+    public function resize()
+    {
+        return $this->render('vue/examples/resize.html.twig');
+    }
+
+    /**
+     * @Route("/examples/scroll", name="vue_examples_scroll")
+     */
+    public function scroll()
+    {
+        return $this->render('vue/examples/scroll.html.twig');
+    }
+
+    /**
+     * @Route("/examples/scroll-progress", name="vue_examples_scroll_progress")
+     */
+    public function scrollProgress()
+    {
+        return $this->render('vue/examples/scroll_progress.html.twig');
+    }
 }
