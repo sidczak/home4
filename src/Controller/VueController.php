ce6d5bec94bbe1c0f29505c12a44f6dcee0f6f3c
Szymon Idczak <szymon.idczak@gmail.com>
2020-03-01 17:51:45 +0100

    sidebar menu vue

--- src/Controller/VueController.php
+++ src/Controller/VueController.php
@@ -18,11 +18,19 @@ class VueController extends AbstractController
         return $this->render('vue/start/index.html.twig');
     }
     
-    // /**
-    //  * @Route("/examples/album", name="vue_examples_album")
-    //  */
-    // public function album()
-    // {
-    //     return $this->render('vue/examples/album.html.twig');
-    // }
+    /**
+     * @Route("/examples/album", name="vue_examples_album")
+     */
+    public function album()
+    {
+        return $this->render('vue/examples/album.html.twig');
+    }
+    
+    /**
+     * @Route("/examples/browser", name="vue_examples_browser")
+     */
+    public function browser()
+    {
+        return $this->render('vue/examples/browser.html.twig');
+    }
 }
