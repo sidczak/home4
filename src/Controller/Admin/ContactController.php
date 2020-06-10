6266bd1cd878b2c3170d79081ec4deecf3e7a010
sidczak <sidczak@clickmeeting.com>
2020-06-10 17:52:10 +0200

    contac pagination

--- src/Controller/Admin/ContactController.php
+++ src/Controller/Admin/ContactController.php
@@ -15,21 +15,42 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/", name="admin_contact_index", methods={"GET"})
+     * @Route("/{page}", name="admin_contact_index", methods={"GET"}, defaults={"page": 1}, requirements={"page" = "\d+"})
      */
-    public function index(): Response
+    public function index(int $page): Response
     {
         $contacts = $this->getDoctrine()
             ->getRepository(Contact::class)
             ->findAll();
 
+        $total_contacts = count($contacts);
+        $contacts_per_page = 10;
+        $last_page = ceil($total_contacts / $contacts_per_page);
+        $previous_page = $page > 1 ? $page - 1 : 1;
+        $next_page = $page < $last_page ? $page + 1 : $last_page;
+        
+        $repository = $this->getDoctrine()
+            ->getRepository(Contact::class);
+
+        $query = $repository->createQueryBuilder('c')
+            ->setFirstResult(($page - 1) * $contacts_per_page)
+            ->setMaxResults($contacts_per_page)
+            ->getQuery();
+
+        $contacts = $query->getResult();
+
         return $this->render('admin/contact/index.html.twig', [
             'contacts' => $contacts,
+            'last_page' => $last_page,
+            'previous_page' => $previous_page,
+            'current_page' => $page,
+            'next_page' => $next_page,
+            'total_contacts' => $total_contacts,
         ]);
     }
 
     /**
-     * @Route("/{id}", name="admin_contact_show", methods={"GET"})
+     * @Route("/{id}/show", name="admin_contact_show", methods={"GET"})
      */
     public function show(Contact $contact): Response
     {
