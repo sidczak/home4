eacf7fe088c67b0642d05d2d3de8e3a0b5699b4a
Szymon Idczak <szymon.idczak@gmail.com>
2020-10-10 15:48:16 +0200

    security user

---
+++ src/Entity/User.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+class User
+{
+    private $id;
+
+    private $email;
+
+    private $roles = [];
+
+    private $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getRoles(): ?array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
