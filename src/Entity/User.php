4655dbbb2cf9d4e81e64efc73e60b98487b67918
Szymon Idczak <szymon.idczak@gmail.com>
2020-10-18 15:30:44 +0200

    security registration form valid

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,8 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+/**
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ */
 class User implements UserInterface
 {
     private $id;
