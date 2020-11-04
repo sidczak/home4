7f584127aae2576a9de8bfa5be264791608939c5
sidczak <sidczak@clickmeeting.com>
2020-11-04 19:55:13 +0100

    waves - block color

--- assets/js/bs-components/Waves.vue
+++ assets/js/bs-components/Waves.vue
@@ -1,30 +1,32 @@
 <template lang="pug">
     section.d-flex.flex-shrink-0.section-min-h-100
         b-container(fluid)
-            b-row.section-py-80.section-min-h-100.align-items-center.bg-clouds
-                b-col
+            b-row.section-py-80.section-min-h-100.card-waves(align-v="center" align-h="center" :class="{ 'bg-turquoise': turquoise, 'bg-orange': orange, 'bg-alizarin': alizarin, 'bg-peter-river': peterRiver, 'bg-clouds': !turquoise && !orange && !alizarin && !peterRiver}")
+                b-col(lg="8")
+                    b-row.text-center
+                        b-col(lg="6")
+                            b-button(size="lg" variant="outline-wet-asphalt" pill)
+                                | Card button
+                        b-col(lg="6")
+                            b-button(size="lg" variant="outline-wet-asphalt" pill)
+                                | Card button
                     b-row
                         b-col.my-3(lg="6")
-                            b-card.border-0(title="Card Title" @mouseover="hover = true" @mouseleave="hover = false" :class="hover ? 'shadow-turquoise-concave-145 shadow-clouds' : 'shadow-clouds-concave-145'")
+                            b-card.border-0(title="Card Title" @mouseover="turquoise = true" @mouseleave="turquoise = false" :class="[turquoise ? 'shadow-turquoise-concave-145' : 'shadow-clouds-concave-145', {'shadow-turquoise': turquoise, 'shadow-orange': orange, 'shadow-alizarin': alizarin, 'shadow-peter-river': peterRiver}]")
                                 b-card-text
                                     | With supporting text below as a natural lead-in to additional content.
                         b-col.my-3(lg="6")
-                            b-card.border-0(title="Card Title" @mouseover="hoverOrange = true" @mouseleave="hoverOrange = false" :class="hoverOrange ? 'shadow-orange-concave-145 shadow-clouds' : 'shadow-clouds-concave-145'")
+                            b-card.border-0(title="Card Title" @mouseover="orange = true" @mouseleave="orange = false" :class="[orange ? 'shadow-orange-concave-225' : 'shadow-clouds-concave-225', {'shadow-turquoise': turquoise, 'shadow-orange': orange, 'shadow-alizarin': alizarin, 'shadow-peter-river': peterRiver}]")
                                 b-card-text
                                     | With supporting text below as a natural lead-in to additional content.
-                        //- b-col.my-3(lg="6")
-                        //-     b-card.shadow-clouds-concave-225.border-0(title="Card Title" @mouseover="mouseover" @mouseleave="mouseleave")
-                        //-         b-card-text
-                        //-             | With supporting text below as a natural lead-in to additional content.
-                        //- b-col.my-3(lg="6")
-                        //-     b-card.shadow-clouds-concave-45.border-0(title="Card Title" @mouseover="mouseover" @mouseleave="mouseleave")
-                        //-         b-card-text
-                        //-             | With supporting text below as a natural lead-in to additional content.
-                        //- b-col.my-3(lg="6")
-                        //-     //- b-card.shadow-turquoise-concave-315.border-0(text-variant="white" title="Card Title")
-                        //-     b-card.shadow-clouds-concave-315.border-0(title="Card Title" @mouseover="mouseover" @mouseleave="mouseleave")
-                        //-         b-card-text
-                        //-             | With supporting text below as a natural lead-in to additional content.
+                        b-col.my-3(lg="6")
+                            b-card.border-0(title="Card Title" @mouseover="alizarin = true" @mouseleave="alizarin = false" :class="[alizarin ? 'shadow-alizarin-concave-45' : 'shadow-clouds-concave-45', {'shadow-turquoise': turquoise, 'shadow-orange': orange, 'shadow-alizarin': alizarin, 'shadow-peter-river': peterRiver}]")
+                                b-card-text
+                                    | With supporting text below as a natural lead-in to additional content.
+                        b-col.my-3(lg="6")
+                            b-card.border-0(title="Card Title" @mouseover="peterRiver = true" @mouseleave="peterRiver = false" :class="[peterRiver ? 'shadow-peter-river-concave-315' : 'shadow-clouds-concave-315', {'shadow-turquoise': turquoise, 'shadow-orange': orange, 'shadow-alizarin': alizarin, 'shadow-peter-river': peterRiver}]")
+                                b-card-text
+                                    | With supporting text below as a natural lead-in to additional content.
 </template>
 
 <script>
@@ -32,12 +34,23 @@ export default {
     name: "Waves",
     data () {
         return {
-            hover: false,
-            hoverOrange:false
+            turquoise: false,
+            orange: false,
+            alizarin: false,
+            peterRiver: false
         }
     }
 };
 </script>
 
 <style lang="scss" scoped>
-    </style>
+.card {
+    transition: all .3s ease-in-out;
+    &:hover {
+        color: #fff;
+    }
+    &-waves {
+        transition: all .3s ease-in-out;
+    }
+}
+</style>
