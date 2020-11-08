506337910eac7feb6e17c50ce2f28b2f0f33d94f
sidczak <sidczak@clickmeeting.com>
2020-11-08 21:19:00 +0100

    register form

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
