4f7c4741163c478f6b8c6ca007d1187316f61d5c
Szymon Idczak <szymon.idczak@gmail.com>
2020-12-21 20:28:42 +0100

    shadow-clouds-concave

--- assets/js/bs-components/Waves.vue
+++ assets/js/bs-components/Waves.vue
@@ -12,20 +12,20 @@
                                 | Card button
                     b-row
                         b-col.my-3(lg="6")
-                            b-card.border-0(title="Card Title" @mouseover="turquoise = true" @mouseleave="turquoise = false" :class="[turquoise ? 'bg-turquoise-concave-145' : 'bg-clouds-concave-145', {'shadow-turquoise-concave-145': turquoise, 'shadow-orange-concave-145': orange, 'shadow-alizarin-concave-145': alizarin, 'shadow-peter-river-concave-145': peterRiver}]")
+                            b-card.border-0(title="Card Title" @mouseover="turquoise = true" @mouseleave="turquoise = false" :class="[turquoise ? 'bg-turquoise-concave-145' : 'bg-clouds-concave-145', {'shadow-turquoise-concave-145': turquoise, 'shadow-orange-concave-145': orange, 'shadow-alizarin-concave-145': alizarin, 'shadow-peter-river-concave-145': peterRiver, 'shadow-clouds-concave-145': !turquoise && !orange && !alizarin && !peterRiver}]")
                                 .card-curtain
                                 b-card-text
                                     | With supporting text below as a natural lead-in to additional content.
                         b-col.my-3(lg="6")
-                            b-card.border-0(title="Card Title" @mouseover="orange = true" @mouseleave="orange = false" :class="[orange ? 'bg-orange-concave-225' : 'bg-clouds-concave-225', {'shadow-turquoise-concave-225': turquoise, 'shadow-orange-concave-225': orange, 'shadow-alizarin-concave-225': alizarin, 'shadow-peter-river-concave-225': peterRiver}]")
+                            b-card.border-0(title="Card Title" @mouseover="orange = true" @mouseleave="orange = false" :class="[orange ? 'bg-orange-concave-225' : 'bg-clouds-concave-225', {'shadow-turquoise-concave-225': turquoise, 'shadow-orange-concave-225': orange, 'shadow-alizarin-concave-225': alizarin, 'shadow-peter-river-concave-225': peterRiver, 'shadow-clouds-concave-225': !turquoise && !orange && !alizarin && !peterRiver}]")
                                 b-card-text
                                     | With supporting text below as a natural lead-in to additional content.
                         b-col.my-3(lg="6")
-                            b-card.border-0(title="Card Title" @mouseover="alizarin = true" @mouseleave="alizarin = false" :class="[alizarin ? 'bg-alizarin-concave-45' : 'bg-clouds-concave-45', {'shadow-turquoise-concave-45': turquoise, 'shadow-orange-concave-45': orange, 'shadow-alizarin-concave-45': alizarin, 'shadow-peter-river-concave-45': peterRiver}]")
+                            b-card.border-0(title="Card Title" @mouseover="alizarin = true" @mouseleave="alizarin = false" :class="[alizarin ? 'bg-alizarin-concave-45' : 'bg-clouds-concave-45', {'shadow-turquoise-concave-45': turquoise, 'shadow-orange-concave-45': orange, 'shadow-alizarin-concave-45': alizarin, 'shadow-peter-river-concave-45': peterRiver, 'shadow-clouds-concave-45': !turquoise && !orange && !alizarin && !peterRiver}]")
                                 b-card-text
                                     | With supporting text below as a natural lead-in to additional content.
                         b-col.my-3(lg="6")
-                            b-card.border-0(title="Card Title" @mouseover="peterRiver = true" @mouseleave="peterRiver = false" :class="[peterRiver ? 'bg-peter-river-concave-315' : 'bg-clouds-concave-315', {'shadow-turquoise-concave-315': turquoise, 'shadow-orange-concave-315': orange, 'shadow-alizarin-concave-315': alizarin, 'shadow-peter-river-concave-315': peterRiver}]")
+                            b-card.border-0(title="Card Title" @mouseover="peterRiver = true" @mouseleave="peterRiver = false" :class="[peterRiver ? 'bg-peter-river-concave-315' : 'bg-clouds-concave-315', {'shadow-turquoise-concave-315': turquoise, 'shadow-orange-concave-315': orange, 'shadow-alizarin-concave-315': alizarin, 'shadow-peter-river-concave-315': peterRiver, 'shadow-clouds-concave-315': !turquoise && !orange && !alizarin && !peterRiver}]")
                                 b-card-text
                                     | With supporting text below as a natural lead-in to additional content.
 </template>
