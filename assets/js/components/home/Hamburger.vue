c2b3d9f7c12ab85bc02e73c78f4cfc4aa4b22670
sidczak <sidczak@clickmeeting.com>
2021-07-22 18:56:38 +0200

    add assets/js/components/home/Hamburger.vue style

--- assets/js/components/home/Hamburger.vue
+++ assets/js/components/home/Hamburger.vue
@@ -1,19 +1,29 @@
 <template lang="pug">
-    div(id="nav-icon3" @click="toggle" :class="{open: isOpen}")
+    div.hamburger(@click="toggle" :class="['hamburger-bg-' + hamburgerBg, 'hamburger-' + option, { 'open': isOpen }]")
         span
         span
         span
-        span
-        //- span
-        //- span
 </template>
 
 <script>
 export default {
     name: "Hamburger",
+    props: {
+        hamburgerBg: {
+            type: String,
+            default: 'turquoise',
+            validator: (value) => ['turquoise', 'white'].indexOf(value) > -1,
+        },
+        option: {
+            type: String,
+            default: 'option1',
+            validator: (value) => ['option1', 'option4'].indexOf(value) > -1,
+        },
+    },
     data: function() {
         return {
             isOpen: false,
+
         };
     },
     methods: {
@@ -26,205 +36,206 @@ export default {
 
 <style lang="scss" scoped>
 @import '../../../css/variables/color.scss';
-#nav-icon1, 
-#nav-icon2, 
-#nav-icon3, 
-#nav-icon4 {
-    width: 60px;
-    height: 45px;
+.hamburger {
+    width: 52px;
+    height: 40px;
     position: relative;
-    margin: 50px auto;
+    margin: auto;
     transform: rotate(0deg);
     transition: .5s ease-in-out;
     cursor: pointer;
-    // border: 1px solid red;
-}
-#nav-icon1 span, 
-#nav-icon3 span, 
-#nav-icon4 span {
-    display: block;
-    position: absolute;
-    height: 9px;
-    width: 100%;
-    background: $turquoise;
-    border-radius: 9px;
-    opacity: 1;
-    left: 0;
-    transform: rotate(0deg);
-    transition: .25s ease-in-out;
-}
+    &-bg-turquoise span{
+        background-color: $turquoise;
+    }
+    &-bg-white span{
+        background-color: $white;
+    }
+    span {
+        
+        display: block;
+        position: absolute;
+        height: 8px;
+        width: 100%;
+        border-radius: 9px;
+        opacity: 1;
+        left: 0;
+        transform: rotate(0deg);
+        transition: .25s ease-in-out;
+    }
+    &-option1 {
+        span {
+            &:nth-child(1) {
+                top: 0px;
+            }
+            &:nth-child(2) {
+                top: 15px;
+            }
+            &:nth-child(3) {
+                top: 30px;
+            }
+        }
+        &.open {
+            span {
+                &:nth-child(1) {
+                    top: 15px;
+                    transform: rotate(135deg);
+                }
+                &:nth-child(2) {
+                    opacity: 0;
+                    left: -60px;
+                }
+                &:nth-child(3) {
+                    top: 15px;
+                    transform: rotate(-135deg);
+                }
+            }
+        }
+    }
+    &-option4 {
+        span {
+            &:nth-child(1) {
+                top: 0px;
+                transform-origin: left center;
+            }
 
-#nav-icon1 span:nth-child(1) {
-  top: 0px;
-}
+            &:nth-child(2) {
+                top: 15px;
+                transform-origin: left center;
+            }
 
-#nav-icon1 span:nth-child(2) {
-  top: 18px;
-}
+            &:nth-child(3) {
+                top: 30px;
+                transform-origin: left center;
+            }
+        }
+        &.open {
+            span {
+                &:nth-child(1) {
+                    transform: rotate(45deg);
+                    top: -3px;
+                    left: 8px;
+                }
 
-#nav-icon1 span:nth-child(3) {
-  top: 36px;
-}
+                &:nth-child(2) {
+                    width: 0%;
+                    opacity: 0;
+                }
 
-#nav-icon1.open span:nth-child(1) {
-  top: 18px;
-  transform: rotate(135deg);
-}
-
-#nav-icon1.open span:nth-child(2) {
-  opacity: 0;
-  left: -60px;
-}
-
-#nav-icon1.open span:nth-child(3) {
-  top: 18px;
-  transform: rotate(-135deg);
-}
-
-/* Icon 2 */
-
-#nav-icon2 {
-}
-
-#nav-icon2 span {
-  display: block;
-  position: absolute;
-  height: 9px;
-  width: 50%;
-  background: $turquoise;
-  opacity: 1;
-  transform: rotate(0deg);
-  transition: .25s ease-in-out;
-}
-
-#nav-icon2 span:nth-child(even) {
-  left: 50%;
-  border-radius: 0 9px 9px 0;
-}
-
-#nav-icon2 span:nth-child(odd) {
-  left:0px;
-  border-radius: 9px 0 0 9px;
-}
-
-#nav-icon2 span:nth-child(1), #nav-icon2 span:nth-child(2) {
-  top: 0px;
-}
-
-#nav-icon2 span:nth-child(3), #nav-icon2 span:nth-child(4) {
-  top: 18px;
-}
-
-#nav-icon2 span:nth-child(5), #nav-icon2 span:nth-child(6) {
-  top: 36px;
-}
-
-#nav-icon2.open span:nth-child(1),#nav-icon2.open span:nth-child(6) {
-  transform: rotate(45deg);
-}
-
-#nav-icon2.open span:nth-child(2),#nav-icon2.open span:nth-child(5) {
-  transform: rotate(-45deg);
+                &:nth-child(3) {
+                    transform: rotate(-45deg);
+                    top: 33px;
+                    left: 8px;
+                }
+            }
+        }
+    }
 }
 
-#nav-icon2.open span:nth-child(1) {
-  left: 5px;
-  top: 7px;
-}
 
-#nav-icon2.open span:nth-child(2) {
-  left: calc(50% - 5px);
-  top: 7px;
-}
+// /* Icon 2 */
+// #nav-icon2 span {
+//   display: block;
+//   position: absolute;
+//   height: 9px;
+//   width: 50%;
+//   background: $turquoise;
+//   opacity: 1;
+//   transform: rotate(0deg);
+//   transition: .25s ease-in-out;
+// }
 
-#nav-icon2.open span:nth-child(3) {
-  left: -50%;
-  opacity: 0;
-}
+// #nav-icon2 span:nth-child(even) {
+//   left: 50%;
+//   border-radius: 0 9px 9px 0;
+// }
 
-#nav-icon2.open span:nth-child(4) {
-  left: 100%;
-  opacity: 0;
-}
+// #nav-icon2 span:nth-child(odd) {
+//   left:0px;
+//   border-radius: 9px 0 0 9px;
+// }
 
-#nav-icon2.open span:nth-child(5) {
-  left: 5px;
-  top: 29px;
-}
+// #nav-icon2 span:nth-child(1), #nav-icon2 span:nth-child(2) {
+//   top: 0px;
+// }
 
-#nav-icon2.open span:nth-child(6) {
-  left: calc(50% - 5px);
-  top: 29px;
-}
+// #nav-icon2 span:nth-child(3), #nav-icon2 span:nth-child(4) {
+//   top: 18px;
+// }
 
-/* Icon 3 */
+// #nav-icon2 span:nth-child(5), #nav-icon2 span:nth-child(6) {
+//   top: 36px;
+// }
 
-#nav-icon3 span:nth-child(1) {
-  top: 0px;
-}
+// #nav-icon2.open span:nth-child(1),#nav-icon2.open span:nth-child(6) {
+//   transform: rotate(45deg);
+// }
 
-#nav-icon3 span:nth-child(2),#nav-icon3 span:nth-child(3) {
-  top: 18px;
-}
+// #nav-icon2.open span:nth-child(2),#nav-icon2.open span:nth-child(5) {
+//   transform: rotate(-45deg);
+// }
 
-#nav-icon3 span:nth-child(4) {
-  top: 36px;
-}
+// #nav-icon2.open span:nth-child(1) {
+//   left: 5px;
+//   top: 7px;
+// }
 
-#nav-icon3.open span:nth-child(1) {
-  top: 18px;
-  width: 0%;
-  left: 50%;
-}
+// #nav-icon2.open span:nth-child(2) {
+//   left: calc(50% - 5px);
+//   top: 7px;
+// }
 
-#nav-icon3.open span:nth-child(2) {
-  transform: rotate(45deg);
-}
+// #nav-icon2.open span:nth-child(3) {
+//   left: -50%;
+//   opacity: 0;
+// }
 
-#nav-icon3.open span:nth-child(3) {
-  transform: rotate(-45deg);
-}
+// #nav-icon2.open span:nth-child(4) {
+//   left: 100%;
+//   opacity: 0;
+// }
 
-#nav-icon3.open span:nth-child(4) {
-  top: 18px;
-  width: 0%;
-  left: 50%;
-}
+// #nav-icon2.open span:nth-child(5) {
+//   left: 5px;
+//   top: 29px;
+// }
 
-/* Icon 4 */
+// #nav-icon2.open span:nth-child(6) {
+//   left: calc(50% - 5px);
+//   top: 29px;
+// }
 
-#nav-icon4 {
-}
+// /* Icon 3 */
+// #nav-icon3 span:nth-child(1) {
+//   top: 0px;
+// }
 
-#nav-icon4 span:nth-child(1) {
-  top: 0px;
-  transform-origin: left center;
-}
+// #nav-icon3 span:nth-child(2),#nav-icon3 span:nth-child(3) {
+//   top: 18px;
+// }
 
-#nav-icon4 span:nth-child(2) {
-  top: 18px;
-  transform-origin: left center;
-}
+// #nav-icon3 span:nth-child(4) {
+//   top: 36px;
+// }
 
-#nav-icon4 span:nth-child(3) {
-  top: 36px;
-  transform-origin: left center;
-}
+// #nav-icon3.open span:nth-child(1) {
+//   top: 18px;
+//   width: 0%;
+//   left: 50%;
+// }
 
-#nav-icon4.open span:nth-child(1) {
-  transform: rotate(45deg);
-  top: -3px;
-  left: 8px;
-}
+// #nav-icon3.open span:nth-child(2) {
+//   transform: rotate(45deg);
+// }
 
-#nav-icon4.open span:nth-child(2) {
-  width: 0%;
-  opacity: 0;
-}
+// #nav-icon3.open span:nth-child(3) {
+//   transform: rotate(-45deg);
+// }
 
-#nav-icon4.open span:nth-child(3) {
-  transform: rotate(-45deg);
-  top: 39px;
-  left: 8px;
-}
+// #nav-icon3.open span:nth-child(4) {
+//   top: 18px;
+//   width: 0%;
+//   left: 50%;
+// }
+
+
 </style>
