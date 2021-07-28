f55571a6da01efaecfd2d50f6de722164d93dbb1
sidczak <sidczak@clickmeeting.com>
2021-07-28 23:28:06 +0200

    add assets/js/components/home/Portfolio.vue 3

--- assets/js/components/home/Portfolio.vue
+++ assets/js/components/home/Portfolio.vue
@@ -16,11 +16,16 @@
                             a.btn.btn-pill.btn-sm.px-3.btn-white(href="#" @click="filter('Webdesign')") Webdesign
                         li.nav-item.m-1
                             a.btn.btn-pill.btn-sm.px-3.btn-white(href="#" @click="filter('Marketing')") Marketing
+                        li.nav-item.m-1
+                            a.btn.btn-pill.btn-sm.px-3.btn-white(href="#" @click="filter('Vue')") Vue
             b-row
                 b-col.col-lg-12
-                    transition-group(name="list-slide" tag="b-row")
+                    transition-group.justify-content-center(name="list-slide" tag="b-row")
                         b-col.list-slide-item(v-for="(item, index) in filteredItems" :key="item" lg="3" md="4" sm="6")
-                            b-card.bg-clouds-concave-145.shadow-clouds-sm.border-0.mb-4(no-body :img-src="require('../../../images/covers/'+ item.id +'.jpg')")
+                            b-card.shadow-clouds-sm.border-0.mb-4(no-body :img-src="require('../../../images/covers/'+ item.id +'.jpg')")
+                            //- b-card.shadow-clouds-sm.border-0.mb-4(no-body :img-src="require('../../../../public/images/portfolio/'+ item.id +'.jpg')")
+                            //- b-card.text-center.shadow-clouds-sm.border-0.mb-4(:class="item.bg ? 'bg-'+ item.bg +'-concave-145 text-white' : 'bg-clouds-concave-145'")
+                                i.fab.fa-fw.fa-2x(:class="'fa-' + icon(item.bg)")
 </template>
 
 <script>
@@ -30,14 +35,14 @@ export default {
     data () {
         return {
             items: [
-                { id: 1, tags: ['all', 'Photography', 'Marketing'] },
-                { id: 2, tags: ['all', 'Identity'] },
+                { id: 1, bg: 'turquoise', tags: ['all', 'Photography', 'Marketing', 'Vue'] },
+                { id: 2, bg: 'turquoise', tags: ['all', 'Identity', 'Vue'] },
                 { id: 3, tags: ['all', 'Identity'] },
-                { id: 4, tags: ['all', 'Photography', 'Identity', 'Marketing', 'Webdesign'] },
+                { id: 4, bg: 'wet-asphalt', tags: ['all', 'Photography', 'Identity', 'Marketing', 'Webdesign'] },
                 { id: 5, tags: ['all', 'Photography'] },
                 { id: 6, tags: ['all', 'Identity'] },
                 { id: 7, tags: ['all', 'Marketing'] },
-                { id: 8, tags: ['all', 'Webdesign'] },
+                { id: 8, bg: 'amethyst', tags: ['all', 'Webdesign'] },
                 // { id: 9, tags: ['all', 'Webdesign'] },
                 // { id: 10, tags: ['all', 'Webdesign'] },
                 // { id: 11, tags: ['all', 'Webdesign'] },
@@ -60,6 +65,14 @@ export default {
         },
         filter: function(tag) {
             this.currentTag = tag;
+        },
+        icon: function(bg) {
+            switch (bg) {
+                case 'turquoise': return 'vuejs'
+                case 'wet-asphalt': return 'symfony'
+                case 'amethyst': return 'bootstrap'
+                default: return 'telegram-plane'
+            }
         }
     }
 };
@@ -80,7 +93,7 @@ export default {
         transform: translateY(-30px);
     }
     &-leave-to {
-        transform: translate(calc(-50% - 10px), 30px);
+        transform: translateY(30px);
     }
     &-leave-active {
         position: absolute;
