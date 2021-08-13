ed306ada3d5651ed0a22c2d3ab5870e8dd0eb56e
sidczak <sidczak@clickmeeting.com>
2021-08-13 15:38:58 +0200

    templates/vue/components/sidebar.html.twig - position

--- assets/js/components/home/Sidebar.vue
+++ assets/js/components/home/Sidebar.vue
@@ -1,88 +1,135 @@
 <template lang="pug">
 b-row
+    b-col(cols="12")
+        .mb-5
+            a.btn.btn-turquoise(@click="show = !show")
+                | button
     b-col
-        .sidebar-menu
-            nav.navbar-expand.d-flex.flex-column.navbar-dark.bg-turquoise-concave-145.shadow-clouds.h-100
-                a.navbar-brand(href="#")
-                    i.fab.fa-fw.fa-vuejs.fa-2x.mb-3
-                ul.navbar-nav.flex-column
-                    li.nav-item
-                        a.nav-link.active(href="#")
-                            i.fas.fa-fw.fa-home
-                            |&nbsp;Start
-                    li.nav-item.dropdown.dropright
-                        a.nav-link.dropdown-toggle(href="#" id="navbarDropdownComponents" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false")
-                            i.fas.fa-fw.fa-tools 
-                            |&nbsp;Components
-                        div.dropdown-menu(aria-labelledby="navbarDropdownComponents")
-                                h6.dropdown-header
-                                    |  Components
-                                a.dropdown-item.disabled(href="#")
-                                    | Alert
-                                a.dropdown-item(href="#")
-                                    | Modal
-                                a.dropdown-item(href="#")
-                                    | Map
-                                a.dropdown-item(href="#")
-                                    | Hamburger
-                                a.dropdown-item(href="#")
-                                    | Portfolio
-                                a.dropdown-item(href="#")
-                                    | Circle Progress
-                    li.nav-item.dropdown.dropright
-                        a.nav-link.dropdown-toggle(href="#" id="navbarDropdownExamples" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false")
-                            i.fas.fa-fw.fa-tools 
-                            |&nbsp;Examples
-                        div.dropdown-menu(aria-labelledby="navbarDropdownExamples")
-                                h6.dropdown-header
-                                    |  Examples
-                                a.dropdown-item(href="#")
-                                    | Album
-                                a.dropdown-item(href="#")
-                                    | Browser
-                ul.navbar-nav.flex-column.mt-auto
-                    li.nav-item
-                        a.nav-link(href="#")
-                            i.fas.fa-fw.fa-tools 
-                            | Portfolio
-                    li.nav-item
-                        a.nav-link(href="#")
-                            i.fas.fa-fw.fa-tools 
-                            | Circle Progress
+        nav.navbar.flex-column.navbar-dark.bg-turquoise-concave-145.h-100.d-inline-flex(:class="[position, {'navbar-expand': show}]")
+            a.navbar-brand.mr-0(href="#")
+                i.fab.fa-fw.fa-vuejs.fa-2x.mb-3
+            ul.navbar-nav.flex-column(:class="{'w-100': !show}")
+                li.nav-item
+                    a.nav-link.active(v-if="!show" href="#")
+                        i.fas.fa-fw.fa-home.mr-1
+                        | Start
+                    a.nav-link.active(v-else href="#" v-b-tooltip.hover.right title="Start")
+                        i.fas.fa-fw.fa-home.mr-1
+                li.nav-item.dropdown.dropright
+                    a.nav-link.dropdown-toggle(href="#" id="navbarDropdownComponents" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false")
+                        i.fas.fa-fw.fa-tools.mr-1
+                        span(:class="{'d-none': show}") Components
+                    div.dropdown-menu(aria-labelledby="navbarDropdownComponents")
+                            h6.dropdown-header
+                                | Components
+                            a.dropdown-item.disabled(href="#")
+                                | Alert
+                            a.dropdown-item(href="#")
+                                | Modal
+                            a.dropdown-item(href="#")
+                                | Map
+                            a.dropdown-item(href="#")
+                                | Hamburger
+                            a.dropdown-item(href="#")
+                                | Portfolio
+                            a.dropdown-item(href="#")
+                                | Circle Progress
+                li.nav-item.dropdown.dropright
+                    a.nav-link.dropdown-toggle(href="#" id="navbarDropdownExamples" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false")
+                        i.fas.fa-fw.fa-tools.mr-1
+                        span(:class="{'d-none': show}") Examples
+                    div.dropdown-menu(aria-labelledby="navbarDropdownExamples")
+                            h6.dropdown-header
+                                |  Examples
+                            a.dropdown-item(href="#")
+                                | Album
+                            a.dropdown-item(href="#")
+                                | Browser
+            ul.navbar-nav.flex-column.mt-auto(:class="{'w-100': !show}")
+                li.nav-item
+                    a.nav-link(v-if="!show" href="#")
+                        i.fas.fa-fw.fa-tools.mr-1
+                        | Portfolio
+                    a.nav-link(v-else href="#" v-b-tooltip.hover.right title="Portfolio")
+                        i.fas.fa-fw.fa-tools.mr-1
+                li.nav-item
+                    a.nav-link(v-if="!show" href="#")
+                        i.fas.fa-fw.fa-tools.mr-1
+                        | Circle Progress
+                    a.nav-link(v-else href="#" v-b-tooltip.hover.right title="Circle Progress")
+                        i.fas.fa-fw.fa-tools.mr-1
     b-col
-        .sidebar-menu
-            nav.navbar-expand.d-flex.flex-column.navbar-light.bg-clouds-concave-145.shadow-clouds.h-100
-                a.navbar-brand(href="#")
-                    i.fab.fa-fw.fa-vuejs.fa-2x.mb-3
-                ul.navbar-nav.flex-column
-                    li.nav-item
-                        a.nav-link.active(href="#")
-                            i.fas.fa-fw.fa-home
-                    li.nav-item
-                        a.nav-link(href="#")
-                            i.fas.fa-fw.fa-tools
-                    li.nav-item
-                        a.nav-link(href="#")
-                            i.fas.fa-fw.fa-tools
-                ul.navbar-nav.flex-column.mt-auto
-                    li.nav-item
-                        a.nav-link(href="#")
-                            i.fas.fa-fw.fa-tools
-                    li.nav-item
-                        a.nav-link(href="#")
-                            i.fas.fa-fw.fa-tools
+        nav.navbar.flex-column.navbar-light.bg-clouds-concave-145.shadow-clouds.h-100.d-inline-flex(:class="{'navbar-expand': show}")
+            a.navbar-brand.mr-0(href="#")
+                i.fab.fa-fw.fa-vuejs.fa-2x.mb-3
+            ul.navbar-nav.flex-column(:class="{'w-100': !show}")
+                li.nav-item
+                    a.nav-link.active(href="#")
+                        i.fas.fa-fw.fa-home.mr-1
+                        span(:class="{'d-none': show}") Start
+                li.nav-item.dropdown.dropright
+                    a.nav-link.dropdown-toggle(href="#" id="navbarDropdownComponents" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false")
+                        i.fas.fa-fw.fa-tools.mr-1
+                        span(:class="{'d-none': show}") Components
+                    div.dropdown-menu(aria-labelledby="navbarDropdownComponents")
+                            h6.dropdown-header
+                                | Components
+                            a.dropdown-item.disabled(href="#")
+                                | Alert
+                            a.dropdown-item(href="#")
+                                | Modal
+                            a.dropdown-item(href="#")
+                                | Map
+                            a.dropdown-item(href="#")
+                                | Hamburger
+                            a.dropdown-item(href="#")
+                                | Portfolio
+                            a.dropdown-item(href="#")
+                                | Circle Progress
+                li.nav-item.dropdown.dropright
+                    a.nav-link.dropdown-toggle(href="#" id="navbarDropdownExamples" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false")
+                        i.fas.fa-fw.fa-tools.mr-1
+                        span(:class="{'d-none': show}") Examples
+                    div.dropdown-menu(aria-labelledby="navbarDropdownExamples")
+                            h6.dropdown-header
+                                |  Examples
+                            a.dropdown-item(href="#")
+                                | Album
+                            a.dropdown-item(href="#")
+                                | Browser
+            ul.navbar-nav.flex-column.mt-auto(:class="{'w-100': !show}")
+                li.nav-item
+                    a.nav-link(href="#")
+                        i.fas.fa-fw.fa-tools.mr-1
+                        span(:class="{'d-none': show}") Portfolio
+                li.nav-item
+                    a.nav-link(href="#")
+                        i.fas.fa-fw.fa-tools.mr-1
+                        span(:class="{'d-none': show}") Circle Progress
 </template>
-<script>    
+<script>
 export default {
     name: "SoundCard",
     props: {
-    }, 
+        position: {
+            type: String,
+            default: '',
+            validator: (value) => ['fixed-left', 'fixed-right', ''].indexOf(value) > -1,
+        },
+    },
+    data () {
+        return {
+            show: true
+        }
+    },
 };
 </script>
 <style lang="scss" scoped>
-.sidebar-menu {
-    width: 160px;
-    height: 400px;
-    margin: auto;
+.flex-left {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    z-index: 1030;
 }
 </style>
