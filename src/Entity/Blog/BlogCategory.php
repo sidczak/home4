19a30543d0bc085617117378df26e74ded46f343
Szymon Idczak <szymon.idczak@gmail.com>
2021-10-30 22:09:27 +0200

    add blog2

--- src/Entity/Blog/BlogCategory.php
+++ src/Entity/Blog/BlogCategory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Entity;
+namespace App\Entity\Blog;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
--- src/Entity/Blog/BlogImage.php
+++ src/Entity/Blog/BlogImage.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Entity;
+namespace App\Entity\Blog;
 
 use Doctrine\ORM\Mapping as ORM;
 
